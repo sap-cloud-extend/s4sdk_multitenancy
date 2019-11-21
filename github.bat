5935c21193d1e63738742726a48d208ffadd4a54
Sonek <Christian.Sonek@itelligence.de>
2019-11-21 16:49:15 +0100

    first commit

---
+++ github.bat
@@ -0,0 +1,6 @@
+echo "# s4sdk_multitenancy" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin git@github.com:sap-cloud-extend/s4sdk_multitenancy.git
+git push -u origin master
